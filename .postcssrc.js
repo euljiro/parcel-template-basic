0d8151687e8a07bdc8e52a676fc8de84dd54f235
iillyy <ma_natsu03@naver.com>
2021-07-04 18:01:16 +0900

    add:parcel

---
+++ .postcssrc.js
@@ -0,0 +1,18 @@
+//ESM > import export
+//node > CommonJs
+
+//import
+// const autoprefixer = require('autoprefixer')
+
+//export
+// module.exports={
+//     plugins: [
+//         autoprefixer
+//     ]
+// }
+
+module.exports={
+    plugins:[
+        require('autoprefixer')
+    ]
+}
